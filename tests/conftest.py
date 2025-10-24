362a2391e6fdac8cfd9e09c74a7bcad9ee0dc83c
Jashan36 <vengalathurjashan369@gmail.com>
2025-10-24 12:14:37 +0530

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import pytest
+import asyncio
+import sys
 
 def pytest_addoption(parser):
     parser.addoption("--run-slow", action="store_true", default=False, help="run slow tests")
@@ -7,6 +9,7 @@ def pytest_addoption(parser):
 def pytest_configure(config):
     config.addinivalue_line("markers", "slow: mark test as slow to run")
     config.addinivalue_line("markers", "web: mark test as web test")
+    config.addinivalue_line("markers", "asyncio: mark test as async")
 
 def pytest_collection_modifyitems(config, items):
     skip_slow = pytest.mark.skip(reason="need --run-slow option to run")
@@ -18,3 +21,14 @@ def pytest_collection_modifyitems(config, items):
         if "web" in item.keywords and not config.getoption("--run-web"):
             item.add_marker(skip_web)
 
+# Add asyncio event loop support
+@pytest.fixture(scope="session")
+def event_loop():
+    """Create an instance of the default event loop for the test session."""
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+pytest_plugins = ("pytest_asyncio",)
